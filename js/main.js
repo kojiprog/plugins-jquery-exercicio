6337775e4c3b73c2198c51ee2705ec6467bd9afd
kojiprog <alexandre.k.ueda@gmail.com>
2024-05-31 18:31:20 -0300

    commit com correção dos bugs

--- js/main.js
+++ js/main.js
@@ -1,42 +1,50 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function () {
+  $("#carousel-imagens").slick({
+    autoplay: true,
+    arrows: false,
+  });
+  $("#telefone").mask("(00) 00000-0000", {
+    placeholder: "(DDD) 12345-6789",
+  });
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+  $("#cpf").mask("000.000.000-00", {
+    placeholder: "123.456.789-00",
+  });
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+  $("#cep").mask("00000-000", {
+    placeholder: "012345-678",
+  });
 
-$('form').validate({
+  $("#form").validate({
     rules: {
-        nome: {
-            requerido: true
-        },
-        email: {
-            requerido: true,
-            email: true
-        },
-        telefone: {
-            requerido: true
-        },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
+      nome: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      telefone: {
+        required: true,
+      },
+      endereco: {
+        required: true,
+      },
+      cep: {
+        required: true,
+      },
+      cpf: {
+        required: true,
+      },
     },
     submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
+      alert(
+        "Sua requisição foi enviada para análise, parabéns pela aquisição!"
+      );
+      form.reset();
     },
     invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
+      alert("Por favor, preencha os campos para prosseguir com a compra!");
+    },
+  });
+});
